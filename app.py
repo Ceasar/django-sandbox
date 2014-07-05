451ab65487df6638ed36f70de524a64b19ed1641
Ceasar Bautista <cbautista2010@gmail.com>
2014-07-05 13:16:12 -0700

    use templates

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
+import os
 import sys
 
 from django.conf import settings
 from django.conf.urls import patterns
-from django.http import HttpResponse
 from django.core.management import execute_from_command_line
+
+BASE_PATH = os.path.dirname(__file__)
 
 settings.configure(
     DEBUG=True,
     SECRET_KEY='placerandomsecretkeyhere',
     ROOT_URLCONF=sys.modules[__name__],
+    TEMPLATE_DIRS=(
+        os.path.join(BASE_PATH, "templates"),
+    )
 )
+
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Powered by Django')
+    return render(request, 'index.html', {'request': request})
 
 urlpatterns = patterns(
     '',

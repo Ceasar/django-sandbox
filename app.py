1ef6164078051c08f464fc40cacdea1ccbe6219b
Ceasar Bautista <cbautista2010@gmail.com>
2014-08-28 15:35:54 -0700

    add admin site

--- app.py
+++ app.py
@@ -4,19 +4,39 @@ import sys
 from django.conf import settings
 from django.conf.urls import patterns
 from django.core.management import execute_from_command_line
+from django.conf.urls import include
 
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 BASE_PATH = os.path.dirname(__file__)
 
 settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    },
     DEBUG=True,
+    INSTALLED_APPS=(
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.staticfiles',
+    ),
     SECRET_KEY='placerandomsecretkeyhere',
+    STATIC_URL='/static/',
     ROOT_URLCONF=sys.modules[__name__],
     TEMPLATE_DIRS=(
         os.path.join(BASE_PATH, "templates"),
     )
 )
 
+
+from django.contrib import admin
 from django.shortcuts import render
+
+admin.autodiscover()
 
 
 def index(request):
@@ -25,6 +45,7 @@ def index(request):
 urlpatterns = patterns(
     '',
     (r'^$', index),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 
